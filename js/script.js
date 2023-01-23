68acb519612bf4b1b82668214d507d5b7b29315c
Gloubiboulgaah <gloubiboulga.sh@gmail.com>
2023-01-23 23:14:50 +0100

    v3

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ erik.src = "music/Eriks_Song.mp3";
 
 var xtal = new Audio();
 xtal.src = "music/Xtal.mp3";
+
+var where_is_my_mind = new Audio();
+where_is_my_mind.src = "music/Where_Is_My_Mind.mp3"
