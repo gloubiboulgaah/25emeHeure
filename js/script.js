8afd6962c0d48fb07d063ab6861c3afbce7d3e76
Gloubiboulgaah <gloubiboulga.sh@gmail.com>
2023-01-28 22:51:48 +0100

    MaJ 28.01.23
    La musique se coupe lorsqu'on en lance une nouvelle

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
-var erik = new Audio();
-erik.src = "music/Eriks_Song.mp3";
+music = new Audio();
 
-var xtal = new Audio();
-xtal.src = "music/Xtal.mp3";
+function erik() {
+    music.pause();
+    music.src = "music/erik.mp3"
+    music.play(); 
+}
 
-var where_is_my_mind = new Audio();
-where_is_my_mind.src = "music/Where_Is_My_Mind.mp3"
+function xtal() {
+    music.pause();
+    music.src = "music/xtal.mp3"
+    music.play();
+}
+// var erik = new Audio();
+// erik.src = "music/Eriks_Song.mp3";
+
+// var xtal = new Audio();
+// xtal.src = "music/Xtal.mp3";
+
+// var where_is_my_mind = new Audio();
+// where_is_my_mind.src = "music/Where_Is_My_Mind.mp3"
+
+// var btn1 = document.getElementById("where_is_my_mind_sound");
+// var music = new Audio();
+
+// btn1.addEventListener("onclick",function(){
+//     where_is_my_mind.play();
+//     music.pause();
+//     music.src = "music/Where_Is_My_Mind.mp3"
+//     music.play();
+// });
+
+function where_is_my_mind() {
+    music.pause();
+    music.src = "music/Where_Is_My_Mind.mp3"
+    music.play();
+}
+
