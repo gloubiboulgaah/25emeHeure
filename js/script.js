bead4d587470d5de2496d1401a6044ea00f6a96e
Gloubiboulgaah <gloubiboulga.sh@gmail.com>
2023-01-28 23:01:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ music = new Audio();
 
 function erik() {
     music.pause();
-    music.src = "music/erik.mp3"
+    music.src = "music/Eriks_Song.mp3"
     music.play(); 
 }
 
@@ -11,24 +11,6 @@ function xtal() {
     music.src = "music/xtal.mp3"
     music.play();
 }
-// var erik = new Audio();
-// erik.src = "music/Eriks_Song.mp3";
-
-// var xtal = new Audio();
-// xtal.src = "music/Xtal.mp3";
-
-// var where_is_my_mind = new Audio();
-// where_is_my_mind.src = "music/Where_Is_My_Mind.mp3"
-
-// var btn1 = document.getElementById("where_is_my_mind_sound");
-// var music = new Audio();
-
-// btn1.addEventListener("onclick",function(){
-//     where_is_my_mind.play();
-//     music.pause();
-//     music.src = "music/Where_Is_My_Mind.mp3"
-//     music.play();
-// });
 
 function where_is_my_mind() {
     music.pause();
